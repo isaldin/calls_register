89114854068021e0be376dc7dc459d2c76c93a12
il.ya sald.in <saldin.iliya@gmail.com>
2014-02-28 21:31:07 +0600

    init app and security

---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ActiveRecord::Base
+  # new columns need to be added here to be writable through mass assignment
+  attr_accessible :username, :phone, :password, :password_confirmation
+
+  attr_accessor :password
+  before_save :prepare_password
+
+  validates_presence_of :username
+  validates_uniqueness_of :username, :phone, :allow_blank => true
+  validates_format_of :username, :with => /\A[a-z]{4,20}\z/, :message => "should only contain letters, numbers, or .-_@"
+  validates_format_of :phone, :with => /\A[0-9]{5}\z/
+  validates_presence_of :password, :on => :create
+  validates_confirmation_of :password
+  validates_length_of :password, :minimum => 4, :allow_blank => true
+
+  def self.authenticate(login, pass)
+    user = find_by_username(login)
+    return user if user && user.password_hash == user.encrypt_password(pass)
+  end
+
+  def encrypt_password(pass)
+    BCrypt::Engine.hash_secret(pass, password_salt)
+  end
+
+  private
+
+  def prepare_password
+    unless password.blank?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = encrypt_password(password)
+    end
+  end
+end
